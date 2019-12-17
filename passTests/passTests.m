f164daf634de09e13239bffdc74efd9b74ad6051
fsdf1151 <liziqiang563@163.com>
2019-12-17 13:47:50 +0800

    2019.12.17

---
+++ passTests/passTests.m
@@ -0,0 +1,37 @@
+//
+//  passTests.m
+//  passTests
+//
+//  Created by 李子强 on 2019/12/17.
+//  Copyright © 2019 李子强. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface passTests : XCTestCase
+
+@end
+
+@implementation passTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
